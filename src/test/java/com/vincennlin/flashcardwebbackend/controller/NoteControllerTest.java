92e78ec3a5a27e20c83ea4c3c6d0eabf1f069e2c
vincennlin <vincentagwa@gmail.com>
2024-06-21 12:57:11 +0800

    筆記功能 － 增加單元測試檔案

---
+++ src/test/java/com/vincennlin/flashcardwebbackend/controller/NoteControllerTest.java
@@ -0,0 +1,66 @@
+package com.vincennlin.flashcardwebbackend.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class NoteControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Test
+    void getAllNotes() throws Exception{
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .get("/api/v1/notes");
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().is(200))
+                .andExpect(jsonPath("$.totalElements", equalTo(5)))
+                .andExpect(jsonPath("$.results[0].content", equalTo("Test note 1")))
+                .andExpect(jsonPath("$.results[0].note_id", notNullValue()))
+                .andExpect(jsonPath("$.results[0].date_created", notNullValue()))
+                .andExpect(jsonPath("$.results[0].last_updated", notNullValue()));
+    }
+
+    @Test
+    void getNoteById() throws Exception{
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .get("/api/v1/notes/{id}", 1);
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().is(200))
+                .andExpect(jsonPath("$.content", equalTo("Test note 1")))
+                .andExpect(jsonPath("$.note_id", notNullValue()))
+                .andExpect(jsonPath("$.date_created", notNullValue()))
+                .andExpect(jsonPath("$.last_updated", notNullValue()));
+    }
+
+    @Test
+    void createNote() {
+    }
+
+    @Test
+    void updateNote() {
+    }
+
+    @Test
+    void deleteNoteById() {
+    }
+}
---
+++ src/test/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO notes (content, date_created, last_updated) VALUES ('Test note 1', '2024-06-21 12:12:00', '2024-06-21 12:12:00');
+
+INSERT INTO notes (content, date_created, last_updated)
+VALUES ('Test note 1 by userId 1', '2024-06-07 13:01:35', '2024-06-07 13:01:35');
