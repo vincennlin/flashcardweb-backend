ae83007189084e87476f9441ef48495fe1f0e70a
vincennlin <vincentagwa@gmail.com>
2024-06-21 10:29:13 +0800

    初始化 Project

---
+++ src/main/java/com/vincennlin/flashcardbackend/FlashcardBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vincennlin.flashcardbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashcardBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlashcardBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vincennlin/flashcardbackend/FlashcardBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vincennlin.flashcardbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashcardBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
