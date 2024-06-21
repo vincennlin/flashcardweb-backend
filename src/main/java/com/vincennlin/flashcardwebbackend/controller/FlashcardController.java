05ba1d1b3f5dca163aa5620663133ec19dc7eb05
vincennlin <vincentagwa@gmail.com>
2024-06-21 18:46:11 +0800

    Swagger OpenAPI － 設定筆記、字卡 CRUD API 的註解

--- src/main/java/com/vincennlin/flashcardwebbackend/controller/FlashcardController.java
+++ src/main/java/com/vincennlin/flashcardwebbackend/controller/FlashcardController.java
@@ -2,6 +2,9 @@ package com.vincennlin.flashcardwebbackend.controller;
 
 import com.vincennlin.flashcardwebbackend.payload.FlashcardDto;
 import com.vincennlin.flashcardwebbackend.service.FlashcardService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +13,11 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(
+        name = "字卡 API",
+        description = "字卡的 CRUD API"
+
+)
 @RestController
 @Validated
 @RequestMapping("/api/v1")
@@ -21,6 +29,14 @@ public class FlashcardController {
         this.flashcardService = flashcardService;
     }
 
+    @Operation(
+            summary = "取得特定筆記的所有字卡",
+            description = "根據 noteId 取得特定筆記的所有字卡"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "成功取得特定筆記的所有字卡"
+    )
     @GetMapping("/notes/{noteId}/flashcards")
     public ResponseEntity<List<FlashcardDto>> getFlashcardsByNoteId(@PathVariable Long noteId) {
 
@@ -29,6 +45,14 @@ public class FlashcardController {
         return new ResponseEntity<>(flashcardsResponse, HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "取得特定字卡",
+            description = "根據 flashcardId 取得特定字卡"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "成功取得特定字卡"
+    )
     @GetMapping("/flashcards/{flashcardId}")
     public ResponseEntity<FlashcardDto> getFlashcardById(@PathVariable Long flashcardId) {
 
@@ -37,6 +61,14 @@ public class FlashcardController {
         return new ResponseEntity<>(flashcardResponse, HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "新增字卡",
+            description = "根據 noteId 新增字卡到特定筆記"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "成功新增字卡"
+    )
     @PostMapping("/notes/{noteId}/flashcards")
     public ResponseEntity<FlashcardDto> createFlashcard(@PathVariable Long noteId,
                                                         @Valid @RequestBody FlashcardDto flashcardDto) {
@@ -46,6 +78,14 @@ public class FlashcardController {
         return new ResponseEntity<>(flashcardResponse, HttpStatus.CREATED);
     }
 
+    @Operation(
+            summary = "更新字卡",
+            description = "根據 flashcardId 更新字卡"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "成功更新字卡"
+    )
     @PutMapping("/flashcards/{flashcardId}")
     public ResponseEntity<FlashcardDto> updateFlashcard(@PathVariable Long flashcardId,
                                                         @Valid @RequestBody FlashcardDto flashcardDto) {
@@ -55,6 +95,14 @@ public class FlashcardController {
         return new ResponseEntity<>(flashcardResponse, HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "刪除字卡",
+            description = "根據 flashcardId 刪除字卡"
+    )
+    @ApiResponse(
+            responseCode = "204",
+            description = "成功刪除字卡"
+    )
     @DeleteMapping("/flashcards/{flashcardId}")
     public ResponseEntity<Void> deleteFlashcardById(@PathVariable Long flashcardId) {
 
--- src/main/java/com/vincennlin/flashcardwebbackend/controller/NoteController.java
+++ src/main/java/com/vincennlin/flashcardwebbackend/controller/NoteController.java
@@ -4,6 +4,9 @@ import com.vincennlin.flashcardwebbackend.payload.NoteDto;
 import com.vincennlin.flashcardwebbackend.payload.NotePageResponse;
 import com.vincennlin.flashcardwebbackend.service.NoteService;
 import com.vincennlin.flashcardwebbackend.utils.AppConstants;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -15,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "筆記 API", description = "筆記的 CRUD API")
 @RestController
 @Validated
 @RequestMapping("/api/v1/notes")
@@ -26,6 +30,14 @@ public class NoteController {
         this.noteService = noteService;
     }
 
+    @Operation(
+            summary = "取得所有筆記",
+            description = "取得所有筆記，並且可以加入分頁、排序等參數"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "成功取得所有筆記"
+    )
     @GetMapping
     public ResponseEntity<NotePageResponse> getAllNotes(
             @RequestParam(name = "pageNo", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER, required = false) Integer pageNo,
@@ -41,6 +53,14 @@ public class NoteController {
         return new ResponseEntity<>(notePageResponse, HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "取得特定筆記",
+            description = "根據 noteId 取得特定筆記"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "成功取得特定筆記"
+    )
     @GetMapping("/{id}")
     public ResponseEntity<NoteDto> getNoteById(@PathVariable Long id) {
 
@@ -49,6 +69,14 @@ public class NoteController {
         return new ResponseEntity<>(note, HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "新增筆記",
+            description = "新增筆記"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "成功新增筆記"
+    )
     @PostMapping
     public ResponseEntity<NoteDto> createNote(@Valid @RequestBody NoteDto noteDto) {
 
@@ -57,6 +85,14 @@ public class NoteController {
         return new ResponseEntity<>(noteResponse, HttpStatus.CREATED);
     }
 
+    @Operation(
+            summary = "更新筆記",
+            description = "根據 noteId 更新筆記"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "成功更新筆記"
+    )
     @PutMapping("/{id}")
     public ResponseEntity<NoteDto> updateNote(@PathVariable Long id,
                                               @RequestBody NoteDto noteDto) {
@@ -66,6 +102,14 @@ public class NoteController {
         return new ResponseEntity<>(noteResponse, HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "刪除筆記",
+            description = "根據 noteId 刪除筆記"
+    )
+    @ApiResponse(
+            responseCode = "204",
+            description = "成功刪除筆記"
+    )
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteNoteById(@PathVariable Long id) {
 
