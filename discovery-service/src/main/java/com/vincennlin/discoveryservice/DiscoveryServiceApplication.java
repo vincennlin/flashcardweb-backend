9db28f388f39a52276f77fe6299322a5bce50e0d
vincennlin <vincentagwa@gmail.com>
2024-07-16 08:57:17 +0800

    Microservice - Enable Eureka Client

--- discovery-service/src/main/java/com/vincennlin/discoveryservice/DiscoveryServiceApplication.java
+++ discovery-service/src/main/java/com/vincennlin/discoveryservice/DiscoveryServiceApplication.java
@@ -2,7 +2,9 @@ package com.vincennlin.discoveryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class DiscoveryServiceApplication {
 
