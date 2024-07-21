270e9af0c589b767d3375eb7c8bfd63b25deef4f
vincennlin <vincentagwa@gmail.com>
2024-07-21 09:22:20 +0800

    Refactor - 刪除原本的 src 資料夾

--- src/main/java/com/vincennlin/flashcardwebbackend/repository/OptionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.vincennlin.flashcardwebbackend.repository;
-
-import com.vincennlin.flashcardwebbackend.entity.flashcard.concrete.Option;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OptionRepository extends JpaRepository<Option, Long> {
-}
