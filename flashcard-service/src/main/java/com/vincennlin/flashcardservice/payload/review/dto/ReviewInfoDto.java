78118249b81755aecdd452055edf2911eb23607c
vincennlin <vincentagwa@gmail.com>
2024-07-27 21:35:27 +0800

    flashcard-ws - Review 單元測試（未完成）

--- flashcard-service/src/main/java/com/vincennlin/flashcardservice/payload/review/dto/ReviewInfoDto.java
+++ flashcard-service/src/main/java/com/vincennlin/flashcardservice/payload/review/dto/ReviewInfoDto.java
@@ -1,23 +1,21 @@
 package com.vincennlin.flashcardservice.payload.review.dto;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
+import java.util.Date;
 
 @Getter
 @Setter
 @AllArgsConstructor
-@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ReviewInfoDto {
 
     public ReviewInfoDto() {
         this.reviewLevel = 0;
         this.reviewInterval = 1;
-        this.nextReview = LocalDateTime.now();
+        this.nextReview = new Date();
     }
 
     @JsonProperty("review_level")
@@ -27,8 +25,8 @@ public class ReviewInfoDto {
     private Integer reviewInterval;
 
     @JsonProperty("last_reviewed")
-    private LocalDateTime lastReviewed;
+    private Date lastReviewed;
 
     @JsonProperty("next_review")
-    private LocalDateTime nextReview;
+    private Date nextReview;
 }
